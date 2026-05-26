b24f3804adad08df217c79d162d4e586896f2c1d
Maciej Mosio <maciej.mosio@op.pl>
2026-05-26 12:40:38 +0200

    Dodalem pionek

---
+++ Pawn.js
@@ -0,0 +1,15 @@
+class Pawn {
+    constructor() {
+        this.type = "PAWN";
+    }
+
+    validateMove(move) {
+
+        // Sprawdzenie warunków poprawnego ruchu pionka
+        if (move.sourceX === move.destinationX && move.destinationY === move.sourceY + 1) {
+            return true; // Poprawny ruch o jedno pole do przodu
+        } else {
+            return false; // Niepoprawny ruch dla pionka
+        }
+    }
+}
